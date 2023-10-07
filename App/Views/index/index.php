77bc165e2ae67e54e825088469e07e88cc14b748
yrllanbrandao <yrllanbrandao@outlook.com>
2023-10-07 16:42:46 -0300

    feat: show shortened link in front-end

--- App/Views/index/index.php
+++ App/Views/index/index.php
@@ -5,13 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Short Links: Encurtar links facilmente</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
   </head>
   <body>
-    <div class="container-fluid p-0 m-0">
+    <div class="container-fluid p-0 m-0 container-main">
+   
+    <form action="" class="form-control d-flex flex-column gap-2 align-items-center border-0" method="post" id="form-link">
+    <img src="./images/image_01.svg" id="img-main">
+        <label for="long-link">Link para encurtamento</label>
+        <input type="text" name="long-link" id="long-link" class="form-control" placeholder="Cole sua Url aqui">
+        <button type="submit" class="btn btn-warning text-light" id="button-short">Encutar link</button>
 
-    <form action="" class="form-control" method="post" id="form-link">
-        <input type="text" name="long-link" id="long-link" class="form-control" placeholder="Insira o link longo">
-        <button type="submit" class="btn btn-warning text-light">Encutar link</button>
+        <div class="container-fluid  p-0 d-flex gap-1" id="link-shorted-area">
+          
+            <input type="text"  id="shorted-link" class="form-control border-primary" readonly>
+            <button class="btn btn-primary">Copiar</button>
+        </div>
     </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,16 @@
 const form = document.getElementById("form-link");
 const inputLink = document.getElementById("long-link");
+const outputLink = document.getElementById("shorted-link");
+const buttonShort = document.getElementById("button-short");
 
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
+    inputLink.readOnly = true;
     const longLink = inputLink.value;
+
+    buttonShort.innerHTML = ". . .";
+    buttonShort.disabled = true;
     shortLink(longLink);
 })
 
@@ -14,18 +20,23 @@ async function shortLink(link){
     const requestBody = {
         'longLink': link
     }
-
-    console.log("----")
-    console.log(requestBody)
-    console.log("----")
     const url =  "http://localhost:8080/short-url";
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(requestBody)
-    })
+   
+    try{
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestBody)
+        })
+        const responseData = await response.json();
 
-    console.log(await response.json());
+        outputLink.value = responseData.shorted_link
+        buttonShort.innerHTML = "Encurtar link";
+        buttonShort.disabled = false;
+    }
+    catch(error){
+        alert("Ocorreu um erro interno, tente novamente mais tarde");
+    }
 }
